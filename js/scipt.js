3dfd828c4454b649754fd9b71f223f9a9ce151c2
Yashwantttt <166321373+Yashwantttt@users.noreply.github.com>
2024-05-22 22:36:02 +0530

    Adding a clear button to clear grid

--- js/scipt.js
+++ js/scipt.js
@@ -24,20 +24,37 @@ function Clicker(){
 
 function gridmaker(x,y){
     let tbl = document.getElementById("container")
+    
+    const newdiv = document.createElement("div");
+    newdiv.classList.add("boxx")
+    newdiv.setAttribute('id',"boxx")
+    tbl.append(newdiv);
+
     for (i=0;i<x;i++){
         let myrow = document.createElement("tr")
         myrow.classList.add("pixels")
         myrow.id = "row" + i 
 
-        tbl.appendChild(myrow);
+        newdiv.appendChild(myrow);
         let roww = document.getElementById("row"+i)
         for (j=0;j<y;j++){
             let mycell = document.createElement("td")
             mycell.classList.add("pixels")
+            mycell.setAttribute('id',"pix")
             roww.appendChild(mycell)
         }
 
     }
+}
+
+
+const clearer = document.getElementById("clear")
+clearer.addEventListener("click",gridRemover)
+
+function gridRemover(){
+    let grid = document.getElementById("boxx")
+    const parent = grid.parentNode
+    parent.removeChild(grid)
 }
 
 // function boxCreater(x){
